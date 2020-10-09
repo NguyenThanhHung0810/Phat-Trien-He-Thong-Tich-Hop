e92c79f40e80b517cb5acc326b5bbc87eb954a11
NguyenThanhHung0810 <72432014+NguyenThanhHung0810@users.noreply.github.com>
2020-10-09 14:10:06 +0700

    Add files via upload

---
+++ allHeaders.java
@@ -0,0 +1,39 @@
+package BT_Tuan3;
+
+
+import java.net.*;
+import java.io.*;
+
+public class allHeaders {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i < 10; i++) 
+			{       
+				try 
+					{          
+						URL u = new URL("https://www.facebook.com/");
+						URLConnection uc = u.openConnection( );
+						for (int j = 1; ; j++) 
+							{            
+								String header = uc.getHeaderField(j);
+								if (header == null) break;
+								System.out.println(uc.getHeaderFieldKey(j) + ": " + header);
+							}
+					}  
+		 
+				catch (MalformedURLException ex)
+					{ 
+						System.err.println("https://www.facebook.com/" + " is not a URL I understand.");
+					}
+				
+				catch (IOException ex)
+					{
+						System.err.println(ex);
+					}
+				System.out.println( ); 
+			}
+		} 
+	}
+
+
